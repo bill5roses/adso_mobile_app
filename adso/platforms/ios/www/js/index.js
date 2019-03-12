63060b57452c104279adeec539877fccf3f427f3
bill5roses <bill@appnexio.com>
2019-03-11 18:16:50 -0700

    v1 app store submits

--- adso/platforms/ios/www/js/index.js
+++ adso/platforms/ios/www/js/index.js
@@ -17,31 +17,102 @@
  * under the License.
  */
 var app = {
-    // Application Constructor
-    initialize: function() {
-        document.addEventListener('deviceready', this.onDeviceReady.bind(this), false);
-    },
+  // Application Constructor
+  initialize: function() {
+    document.addEventListener('deviceready', this.onDeviceReady.bind(this), false);
 
-    // deviceready Event Handler
+    var websiteHolder = document.getElementById("websiteHolder");
+    var website = document.getElementById("website");
+    var closeWebsite = document.getElementById("closeWebsite");
+    var spinContainer = document.getElementById("spinContainer");
+    var showing = false;
+
+    function load(url) {
+      website.src = url;
+      spinContainer.style.display = "block";
+    };
+
+    function show() {
+      if (!showing) {
+        websiteHolder.style.display = "block";
+        closeWebsite.style.display = "block";
+        showing = true;
+        spinContainer.style.display = "none";
+      }
+      websiteHolder.style.width = (window.innerWidth - 10) + "px";
+      websiteHolder.style.height = (window.innerHeight - 80 - 10) + "px";
+      window.setTimeout(function() {
+        websiteHolder.style.width = window.innerWidth + "px";
+        websiteHolder.style.height = (window.innerHeight - 80) + "px";
+      }, 100);
+    };
+
+    function hide() {
+      websiteHolder.style.display = "none";
+      closeWebsite.style.display = "none";
+      website.src = "about:blank";
+      website.contentWindow.document.open();
+      website.contentWindow.document.write("");
+      website.contentWindow.document.close();
+      window.setTimeout(function(){
+        showing = false;
+      }, 50);
+    };
+
+    closeWebsite.addEventListener("click", function(){
+      hide();
+    });
+
+    document.getElementById("news").addEventListener("click", function(){
+      load("https://www.theadso.org/news-2");
+    });
+
+    document.getElementById("education").addEventListener("click", function(){
+      load("https://www.pathlms.com/adso");
+    });
+
+    document.getElementById("careers").addEventListener("click", function(){
+      load("https://www.theadso.org/careers/");
+    });
+
+    document.getElementById("advocacy").addEventListener("click", function(){
+      load("https://www.theadso.org/advocacy-2/");
+    });
+
+    document.getElementById("highlights").addEventListener("click", function(){
+      load("https://www.theadso.org/community-highlights");
+    });
+
+    document.getElementById("events").addEventListener("click", function(){
+      load("https://www.theadso.org/events/");
+    });
+
+    website.addEventListener('load', function() {
+        show();
+        console.log('website loaded');
+    })
+  },
+
+  // deviceready Event Handler
+  //
+  // Bind any cordova events here. Common events are:
+  // 'pause', 'resume', etc.
+  onDeviceReady: function() {
+    this.receivedEvent('deviceready');
+    // window.location.href = "https://www.theadso.org";
+  },
+
+  // Update DOM on a Received Event
+  receivedEvent: function(id) {
+    // var parentElement = document.getElementById(id);
+    // var listeningElement = parentElement.querySelector('.listening');
+    // var receivedElement = parentElement.querySelector('.received');
     //
-    // Bind any cordova events here. Common events are:
-    // 'pause', 'resume', etc.
-    onDeviceReady: function() {
-        this.receivedEvent('deviceready');
-        window.location.href = "https://www.theadso.org";
-    },
+    // listeningElement.setAttribute('style', 'display:none;');
+    // receivedElement.setAttribute('style', 'display:block;');
 
-    // Update DOM on a Received Event
-    receivedEvent: function(id) {
-        // var parentElement = document.getElementById(id);
-        // var listeningElement = parentElement.querySelector('.listening');
-        // var receivedElement = parentElement.querySelector('.received');
-        //
-        // listeningElement.setAttribute('style', 'display:none;');
-        // receivedElement.setAttribute('style', 'display:block;');
-
-        console.log('Received Event: ' + id);
-    }
+    console.log('Received Event: ' + id);
+  }
 };
 
 app.initialize();
